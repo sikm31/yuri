4077e354f26f8ff262c1aaca98c693ae1a98c7b1
sikm31 <sikmroot@gmail.com>
2013-10-10 18:07:22 +0300

    gallery

---
+++ gallery/photo/models.py
@@ -0,0 +1,45 @@
+from django.contrib.sites import admin
+from django.db import models
+# -*- coding: utf-8 -*-
+# Create your models here.
+from django.db.models import permalink
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return self.name
+
+    @permalink
+    def get_absolute_url(self):
+        return ('item_detail', None, {'object_id': self.id})
+
+class Photo(models.Model):
+    item = models.ForeignKey(Item)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='photos')
+    caption = models.CharField(max_length=250, blank=True)
+
+    class Meta:
+        ordering =['title']
+
+    def __unicode__(self):
+        return self.title
+
+    @permalink
+    def get_absolute_url(self):
+        return ('photo_detail', None, {'object_id': self.id})
+
+class PhotoInline(admin.StackedInline):
+    model = Photo
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Photo)
