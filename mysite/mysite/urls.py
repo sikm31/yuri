60911a64893bd190644656b6f2513187a8a16828
sikm31 <sikmroot@gmail.com>
2013-10-29 16:13:25 +0200

    devwsgi with apache mysite apache working site

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('blog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -14,20 +14,33 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+##import os
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+##os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+##from django.core.wsgi import get_wsgi_application
+##application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+import os, sys
+#sys.path.append('/usr/local/lib/python2.7/dist-packages/django/contrib/admin/static')
+sys.path.append('/home/yuri/python')
+sys.path.append('/home/yuri/python/mysite')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+import django.core.handlers.wsgi
+
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+    environ['PATH_INFO'] = environ['SCRIPT_NAME'] + environ['PATH_INFO']
+    return _application(environ, start_response)
