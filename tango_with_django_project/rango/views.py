e370698531c543be06ca2cbbad6f8bd8cee09b7c
sikm31 <sikmroot@gmail.com>
2014-03-27 16:55:27 +0200

    form

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,19 +5,29 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 #def index(request):
 #    context = RequestContext(request)
 #    context_dict = {'boldmessage': "I am bold font from the context"}
 #    return render_to_response('rango/index.html', context_dict, context)
 
+def encode_url(str):
+    return str.replace(' ', '_')
+
+def decode_url(str):
+    return str.replace('_', ' ')
+
 def index(request):
     context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
+    #for category in category_list:
+    #    category.url = category.name.replace(' ','_')
     for category in category_list:
-        category.url = category.name.replace(' ','_')
+        category.url = encode_url(category.name)
     return  render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
@@ -27,14 +37,31 @@ def about(request):
 
 def category(request, category_name_url):
     context = RequestContext(request)
-    category_name = category_name_url.replace('_', ' ')
+    #category_name = category_name_url.replace('_', ' ')
+    category_name = decode_url(category_name_url)
     context_dict = {'category_name': category_name}
     try:
-        category = Category.objects.getl(name=category_name)
-        pages = Page.objects.filter(category=category)
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
         pass
 
     return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form': form}, context)
